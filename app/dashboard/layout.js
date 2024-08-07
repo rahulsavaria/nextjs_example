3ed6e0144216d596e9d77d247838dfdf467838a4
rahulsavaria <rahul.savaria@qed42.com>
2024-08-07 16:31:20 +0530

    build: Adding storybook

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,6 +1,6 @@
 // /components/DashboardLayout.js
 
-import React from 'react ';
+import React from 'react';
 
 export default function DashboardLayout({ children }) {
   return <section className="testing class">{children}</section>;
